99531974e9c6684115d3865ecaf8f9dffbed3071
jonsi <jonsi@DESKTOP-VI5FVR6>
2022-06-02 15:06:16 +0400

    Добавьте файлы проекта.

---
+++ HW9.2/HW9.6.2.cs
@@ -0,0 +1,75 @@
+﻿//Создайте консольное приложение, в котором будет происходить сортировка списка фамилий из пяти человек.
+//Сортировка должна происходить при помощи события.
+//Сортировка происходит при введении пользователем либо числа 1 (сортировка А-Я), либо числа 2 (сортировка Я-А).
+
+//Дополнительно реализуйте проверку введённых данных от пользователя через конструкцию
+//TryCatchFinally с использованием собственного типа исключения.
+
+
+NumberReader numberReader = new NumberReader();
+numberReader.NumberReaderEvent += SortSecondName;
+while (true)
+{
+    try
+    {
+        numberReader.Read();
+    }
+
+    catch (FormatException)
+    {
+        Console.WriteLine("Введено некорректное значение");
+        Console.WriteLine();
+    }
+}
+
+static void SortSecondName(int number)
+{
+    string[] users = new string[5];
+    users[0] = "Арбузиков";
+    users[1] = "Помидорчиков";
+    users[2] = "Дынькин";
+    users[3] = "Клубничкин";
+    users[4] = "Путин";
+
+    switch (number)
+    {
+        case 1:
+            Array.Sort(users);
+            break;
+
+        case 2:
+            Array.Sort(users);
+            Array.Reverse(users);
+            break;
+    }
+
+    foreach (string user in users)
+    {
+        Console.WriteLine(user);
+        
+    }
+    Console.WriteLine();
+}
+
+class NumberReader
+{
+    public delegate void NumberReaderDelegate(int number);
+    public event NumberReaderDelegate NumberReaderEvent;
+
+    public void Read()
+    {
+        Console.WriteLine("Введите число 1 - для сортировки от А-Я или 2 для сортировки от Я-А");
+
+        int numberSort = Convert.ToInt32(Console.ReadLine());
+
+        if (numberSort != 1 && numberSort != 2) throw new FormatException();
+
+        NumberEntered(numberSort);
+
+    }
+
+    protected virtual void NumberEntered(int number)
+    {
+        NumberReaderEvent?.Invoke(number);
+    }
+}
---
+++ HW9/HW9.6.1.cs
@@ -0,0 +1,55 @@
+﻿// 1 Создайте свой тип исключения.
+
+// 2 Сделайте массив из пяти различных видов исключений, включая собственный тип исключения.
+// Реализуйте конструкцию TryCatchFinally, в которой будет итерация на каждый тип исключения (блок finally по желанию).
+
+// 3 В блоке catch выведите в консольном сообщении текст исключения.
+
+
+Exception[] exceptions = new Exception[5];
+exceptions[0] = new MyException();
+exceptions[1] = new DivideByZeroException();
+exceptions[2] = new IndexOutOfRangeException();
+exceptions[3] = new OverflowException();
+exceptions[4] = new RankException();
+
+
+foreach (Exception ex in exceptions)
+{
+    try
+    {
+        throw ex;
+    }
+    catch (Exception e) when (e is MyException)
+    {
+        Console.WriteLine(e.Message);
+    }
+    catch (Exception e) when (e is DivideByZeroException)
+    {
+        Console.WriteLine(e.Message);
+    }
+    catch (Exception e) when (e is IndexOutOfRangeException)
+    {
+        Console.WriteLine(e.Message);
+    }
+    catch (Exception e) when (e is OverflowException)
+    {
+        Console.WriteLine(e.Message);
+    }
+    catch (Exception e) when (e is RankException)
+    {
+        Console.WriteLine(e.Message);
+    }
+
+}
+class MyException : Exception
+{
+    public MyException()
+    {
+
+    }
+    public MyException(string message) : base(message)
+    {
+
+    }
+}
